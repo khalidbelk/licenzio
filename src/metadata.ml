bf3e5b64a64d5840aa29907811b1074877bc2d7d
khalidbelk <me@khal1d.com>
2026-08-29 20:02:33 +0200

    feat: add repo url to the license footer and reuse metadata across modules

---
+++ src/metadata.ml
@@ -0,0 +1,10 @@
+(*
+  licenzio - A CLI tool for adding license files to projects
+  Khalid B. | @khalidbelk, 2026
+  File: src/metadata.ml
+*)
+
+let version = "0.2.1"
+let title = "Licenzio"
+let description = "A CLI tool for adding license files to projects"
+let repo_url = "https://github.com/khalidbelk/licenzio"
