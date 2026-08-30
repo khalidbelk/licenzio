00cf89137e0a3bec7a892ccfce3cc7edec3dc238
khalidbelk <me@khal1d.com>
2026-08-30 15:49:12 +0200

    chore: bump version to 0.2.2

--- src/metadata.ml
+++ src/metadata.ml
@@ -4,7 +4,7 @@
   File: src/metadata.ml
 *)
 
-let version = "0.2.1"
+let version = "0.2.2"
 let title = "Licenzio"
 let description = "A CLI tool for adding license files to projects"
 let repo_url = "https://github.com/khalidbelk/licenzio"
